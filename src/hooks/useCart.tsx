20e5270c93ce0bc7907850a436c06cc619b42ff8
Lucas Fontes <lfonteesc@gmail.com>
2021-09-26 20:24:21 -0300

    Add and remove product

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,18 +23,50 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      console.log(storagedCart);
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  function cartExists(id: number) {
+    return cart.some(function (el) {
+      return el.id === id;
+    });
+  }
+
   const addProduct = async (productId: number) => {
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
     try {
-      // TODO
+      api.get(`/products/${productId}`).then((response) => {
+        if (response.data) {
+          if (!cartExists(response.data.id)) {
+            setCart([...cart, { ...response.data, amount: 1 }]);
+            // localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+          } else {
+            // let newCart = [...cart];
+            // cart.map((product, index) => {
+            //   if (product.id === response.data.id) {
+            //     newCart[index].amount += 1;
+            //   }
+            // });
+            // setCart(newCart);
+
+            const newCart = cart.map((product) => {
+              if (product.id === response.data.id) {
+                return { ...product, amount: (product.amount += 1) };
+              } else {
+                return product;
+              }
+            });
+            setCart(newCart);
+          }
+        }
+      });
     } catch {
       // TODO
     }
@@ -42,7 +74,10 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const newCart = cart.filter((product: Product) => {
+        return product.id !== productId;
+      });
+      setCart(newCart);
     } catch {
       // TODO
     }
